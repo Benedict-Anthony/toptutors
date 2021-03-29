e321d316c0c7c6b3e5d7f2c1e0b4f580702450d9
Hilary <israel.hilary7@yahoo.com>
2021-03-29 22:23:36 +0100

    added tutors routes

---
+++ Route/tutors.route.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const checkAuth = require("../middleware/check-auth")
+const router = express.Router();
+var dbTutors = require("../model/tutors.model");
+
+
+router.get("/", (req, res) => {
+  res.send({
+    message: "message sending",
+  });
+});
+
+router.post("/signup", (req, res) => {
+    dbTutors
+    .findOne({ email: req.body.email })
+    .then((resp) => {
+        console.log(resp)
+      const users = new dbTutors({ ...req.body });
+      users
+        .save()
+        .then(() => {
+          res.status(201).json({
+            message: "Tutor created",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json({
+            message: "Tutor creation failed",
+            error: err,
+          });
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({
+        message: "This email already exist",
+        error,
+      });
+    });
+});
+
+router.get("/:token", (req, res) => {
+  console.log(req.params.token)
+  dbTutors
+    .findOne({ token: req.params.token })
+    .then((user) => {
+        console.log(user)
+        res.send(200, {
+          message: "Successfully retrieved user information",
+          id: user.id,
+          user:user,
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({
+        message: "This token does not exist",
+        error,
+      });
+    });
+});
+
+router.post("/login", (req, res) => {
+    var { email, password } = req.body;
+    dbTutors
+      .findOne({ email })
+      .then((doc) => {
+        doc.comparePassword(password, function (err, isMatch) {
+          if (err) throw err;
+          if (isMatch) {
+            doc.generateToken((error, user) => {
+              console.log(error);
+              if (error) return res.send(400, error);
+              res.set("token", user.token);
+              res.send(200, {
+                message: "User loggedIn",
+                id: user.id,
+                token:user.token,
+              });
+            });
+          } else {
+            res.send(400, {
+              message: "Auth failed password incorrect",
+            });
+          }
+        });
+      })
+      .catch((error) => {
+        res.json({
+          message: "logged in failed user not found",
+          error,
+        });
+      });
+  });
+
+module.exports = router;
